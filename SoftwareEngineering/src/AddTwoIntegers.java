f0cacdd056353dfeb749b4617fcd2af32e18e1e4
beny11867 <53544771+beny11867@users.noreply.github.com>
2020-04-30 22:01:11 +1200

    hope this works

--- SoftwareEngineering/src/AddTwoIntegers.java
+++ SoftwareEngineering/src/AddTwoIntegers.java
@@ -1,13 +1,73 @@
+import java.util.Scanner;
 
 public class AddTwoIntegers {
 
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
+        Scanner scan = new Scanner(System.in);
+        int first = 0;
+        int second = 0;
+        String plus = "+";
+        String minus = "-";
+        String divide = "/";
+        String multiply = "*";
+        String function;
         
-        int first = 100;
-        int second = 300;
+        String response;
+        
+        System.out.println("welcome to the number calculator");
+        
+        for(int I = 0; I != 1;)
+        {
+        
+        System.out.println("Please enter first number: ");
+        first = scan.nextInt();
+        System.out.println("Please enter second number: ");
+        second = scan.nextInt();
+        System.out.println("Now enter a function + for plus, - for minus, / for divide and * for multiply");
+        function = scan.next();
+        
+        if(function.equals("+"))
+        {
+        	int sum = first + second;
 
-        int sum = first + second;
+            System.out.println("The sum is: " + sum);
+        }
+        else if(function.equals("-"))
+        {
+        	int sum = first - second;
 
-        System.out.println("The sum is: " + sum);
+            System.out.println("The sum is: " + sum);
+        	
+        }
+        else if(function.equals("/"))
+        {
+        	int sum = first / second;
+
+            System.out.println("The sum is: " + sum);
+        	
+        }
+        else if(function.equals("*"))
+        {
+        	int sum = first * second;
+
+            System.out.println("The sum is: " + sum);
+        	
+        }
+        else
+        {
+        	System.out.println("Invalid input, restart and try again.");
+        }
+        
+        System.out.println("would you like to go again? (Y/N): ");
+        response = scan.next();
+        
+        if(response.equals("n")|| response.equals("N"))
+        {
+        	I = 1;
+        	System.out.println("goodbye");
+        }
+        }
+
     }
 }
